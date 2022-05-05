2c3cf51a819ff00ce79d446be2f8db26608cb02d
55746 <Lawhead.gavin@live.jobcorps.org>
2022-05-05 19:33:17 +0000

    first commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,58 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <>
+      <nav className="navbar navbar-light bg-light">
+        <img src="src/front/img/3396e642ba214821b01dde9d23292526.png" />
+        <div className="container">
+          <Link to="/">
+            <span className="navbar-brand mb-0 h1">Louis Alert</span>
+          </Link>
+          <div className="ml-auto">
+            <Link to="/demo">
+              <button className="btn btn-primary">Contact</button>
+            </Link>
+            <Link to="/create">
+              <button className="btn btn-primary">Create</button>
+            </Link>
+            <button
+              className="fa fa-share-alt"
+              style={{ fontSize: "36px" }}
+            ></button>
+          </div>
+        </div>
+
+        <div>
+          <ul className="nav justify-content-center">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                example
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                example
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                example
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link disabled"
+                href="#"
+                tabIndex="-1"
+                aria-disabled="true"
+              >
+                example
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,34 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      <div className="card text-center">
+        <div className="card-body">
+          <h1 className="card-title">LOUIS ALERT</h1>
+          <p className="card-text">Public missing brodcast alert system</p>
+          <a href="#" className="btn btn-primary">
+            Missing Persons Page
+          </a>
+        </div>
+      </div>
+      <div>
+        <h2> Sign up to report missing persons</h2>
+        <Link to="/signUp">
+          <button type="button" className="btn btn-primary">
+            Sign up
+          </button>
+        </Link>
+      </div>
+      <div className="alert alert-info">
+        {store.message ||
+          "make sure to make a loading screen or img when the website is loading"}
+      </div>
+    </div>
+  );
 };
