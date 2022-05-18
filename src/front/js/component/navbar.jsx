3acfd27bb096e6d00d42d7dd2b34a441690414bb
55746 <Lawhead.gavin@live.jobcorps.org>
2022-05-18 19:08:58 +0000

    moved the signin button

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -7,9 +7,6 @@ export const Navbar = () => {
       <nav className="navbar navbar-light text-light bg-dark">
         {/* <img src="src/front/img/3396e642ba214821b01dde9d23292526.png" /> */}
         <div className="container">
-          <Link to="/signUp">
-            <button className="navbar-brand">Signup</button>
-          </Link>
           <Link to="/">
             <span className="navbar-brand mb-0 h1 text-light">Destiny</span>
           </Link>
@@ -21,6 +18,9 @@ export const Navbar = () => {
               className="fa fa-share-alt"
               style={{ fontSize: "36px" }}
             ></button>
+            <Link to="/signUp">
+              <button className="navbar-brand">Signup</button>
+            </Link>
           </div>
         </div>
 
--- src/front/js/component/pages/home.js
+++ src/front/js/component/pages/home.js
@@ -8,43 +8,43 @@ export const Home = () => {
   console.log(store.user);
   return (
     <div className="text-center mt-5">
-      {store.user.email ? (
-        <div>
-          <>
-            <div
-              style={{
-                position: "absolute",
-                left: "40%",
-                marginTop: "15%",
-                width: "50%",
-              }}
-              className="input-group"
+      {/* {store.user.email ? ( */}
+      <div>
+        <>
+          <div
+            style={{
+              position: "absolute",
+              left: "40%",
+              marginTop: "15%",
+              width: "50%",
+            }}
+            className="input-group"
+          >
+            <div className="form-outline">
+              <input type="search" id="form1" className="form-control" />
+              <label className="form-label" for="form1"></label>
+            </div>
+            <button
+              type="button"
+              className="btn btn-primary"
+              style={{ position: "absolute", left: "25%" }}
             >
-              <div className="form-outline">
-                <input type="search" id="form1" className="form-control" />
-                <label className="form-label" for="form1"></label>
-              </div>
-              <button
-                type="button"
-                className="btn btn-primary"
-                style={{ position: "absolute", left: "25%" }}
-              >
-                search
-              </button>
-            </div>
-            <div>
-              <img
-                src={Marcus}
-                className="img-fluid"
-                style={{ width: "100%" }}
-                alt="..."
-              />
-            </div>
-          </>
-        </div>
-      ) : (
+              search
+            </button>
+          </div>
+          <div>
+            <img
+              src={Marcus}
+              className="img-fluid"
+              style={{ width: "100%" }}
+              alt="..."
+            />
+          </div>
+        </>
+      </div>
+      {/* ) : (
         <h1>Not Logged in</h1>
-      )}
+      )} */}
     </div>
   );
 };
