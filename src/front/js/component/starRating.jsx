65b5cc42e33542d37eb5a9611025c8e1490f962c
marcuslouis <Louis.Marcus@live.jobcorps.org>
2022-05-20 13:58:23 +0000

    star

--- src/front/js/component/starRating.jsx
+++ src/front/js/component/starRating.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 const StarRating = () => {
+  const [rating, setrating] = useState(null);
   return (
     <div>
       {[...Array(5)].map((star, i) => {
         const ratingValue = i + 1;
         return (
           <label>
-            <input type="radio" name="rating" value={ratingValue} />
-            <FaStar className="star" size={100} />
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setrating(ratingValue)}
+            />
+            <FaStar
+              className="star"
+              color={ratingValue <= rating ? "#ffc107" : "#e4e5e9"}
+              size={100}
+            />
           </label>
         );
       })}
