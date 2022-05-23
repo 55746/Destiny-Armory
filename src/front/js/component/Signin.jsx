469424399f0616f1517c01f57df58dc33764e0d5
marcuslouis <Louis.Marcus@live.jobcorps.org>
2022-05-23 14:33:34 +0000

    user created

--- src/front/js/component/Signin.jsx
+++ src/front/js/component/Signin.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { useHistory } from "react-router-dom";
 
@@ -11,60 +12,65 @@ export const SignIn = () => {
   let history = useHistory();
 
   return (
-    <form>
-      <div className="mb-3">
-        <label for="exampleInputEmail1" className="form-label">
-          Email address
-        </label>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-        <div id="emailHelp" className="form-text">
-          LETS LOGIN
+    <div>
+      <form>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            LETS LOGIN
+          </div>
         </div>
-      </div>
-      <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">
-          PASSWORD
-        </label>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-        />
-      </div>
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" for="exampleCheck1">
-          Check me out
-        </label>
-      </div>
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          if (email === "" || password === "") {
-            alert("The input cannot be empty");
-          } else {
-            actions.signIn(email, password);
-            history.push("/home");
-          }
-        }}
-        formtype="submit"
-        className="btn btn-primary"
-      >
-        Submit
-      </button>
-    </form>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            PASSWORD
+          </label>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            if (email === "" || password === "") {
+              alert("The input cannot be empty");
+            } else {
+              actions.signIn(email, password);
+              history.push("/home");
+            }
+          }}
+          formtype="submit"
+          className="btn btn-primary"
+        >
+          Submit
+        </button>
+      </form>
+      <Link to="/">
+        <button className="btn btn-primary">Back home</button>
+      </Link>
+    </div>
   );
 };
