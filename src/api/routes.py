dd5cff2e6ac8e02914d26c5424ecf739a163eb5f
55746 <Lawhead.gavin@live.jobcorps.org>
2022-05-24 15:35:48 +0000

    started on making the list useable through routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -15,7 +15,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, ExoticWeapon, LegendaryWeapon, RareWeapon, CommonWeapon, UncommonWeapon
+from api.models import db, User, ExoticWeapon, LegendaryWeapon, RareWeapon, CommonWeapon, UncommonWeapon, StarRating
 from api.utils import generate_sitemap, APIException
 
 """
@@ -64,25 +64,46 @@ def get_user():
 @api.route('/signup', methods=['POST'])
 def create_user():
     response_body= request.get_json()
-    print(response_body)
-    if 'email' not in response_body:
-        raise APIException('bad request, email needed', status_code=400)
-    if 'password' not in response_body:
-        raise APIException('bad request, password needed', status_code=400)
-    if 'first_name' not in response_body:
-        raise APIException('bad request, first_name needed', status_code= 400)
-    if 'last_name' not in response_body:
-        raise APIException('bad request, last_name needed', status_code= 400)
-    if 'dob' not in response_body:
-        raise APIException('bad request, Date of birth needed', status_code= 400)
-    new_user=User(email=response_body['email'], password=response_body['password'],first_name=response_body['first_name'],last_name=response_body['last_name'], dob=response_body['dob'])
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify(new_user.serialize()), 200
+
+    if response_body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+        print(response_body)
+    if type(response_body)==dict:
+        if 'email' not in response_body:
+            raise APIException('bad request, email needed', status_code=400)
+        if 'password' not in response_body:
+            raise APIException('bad request, password needed', status_code=400)
+        if 'first_name' not in response_body:
+            raise APIException('bad request, first_name needed', status_code= 400)
+        if 'last_name' not in response_body:
+            raise APIException('bad request, last_name needed', status_code= 400)
+        if 'dob' not in response_body:
+            raise APIException('bad request, Date of birth needed', status_code= 400)
+        new_user=User(email=response_body['email'], password=response_body['password'],first_name=response_body['first_name'],last_name=response_body['last_name'], dob=response_body['dob'])
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify(new_user.serialize()), 200
+    elif type(response_body)==list:
+        for item in response_body:
+            if 'email' not in item:
+                raise APIException('bad request, email needed', status_code=400)
+            if 'password' not in item:
+                raise APIException('bad request, password needed', status_code=400)
+            if 'first_name' not in item:
+                raise APIException('bad request, first_name needed', status_code= 400)
+            if 'last_name' not in item:
+                raise APIException('bad request, last_name needed', status_code= 400)
+            if 'dob' not in item:
+                raise APIException('bad request, Date of birth needed', status_code= 400)
+            new_user=User(email=item['email'], password=item['password'],first_name=item['first_name'],last_name=item['last_name'], dob=item['dob'])
+            db.session.add(new_user)
+            db.session.commit()
+        return "All users were added", 200
 
 @api.route('/exotics', methods=['GET'])
 def exotic_get():
     weapons=ExoticWeapon.query.all()
+    weapons_list=list(map(lambda x: x.serialize(), weapons))
     weapons_list=list(map(lambda x: x.serialize(), weapons))
     return jsonify(weapons_list), 200
 
@@ -95,10 +116,20 @@ def getexoticsId(exotics_id):
 @api.route('/exotics', methods=['POST'])
 def access_exoticweapons():
     response_body = request.get_json()
-    exoticWeapons=ExoticWeapon(weapon_name=response_body['weapon_name'], weapon_type=response_body['weapon_type'],weapon_class=response_body['weapon_class'], weapon_lore=response_body['weapon_lore'], location_description=response_body['location_description'])
-    db.session.add(exoticWeapons)
-    db.session.commit()
-    return jsonify(exoticWeapons.serialize()), 200
+    if response_body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+        print(response_body)
+    if type(response_body)==dict:
+        exoticWeapons=ExoticWeapon(weapon_name=response_body['weapon_name'], weapon_type=response_body['weapon_type'],weapon_class=response_body['weapon_class'], weapon_lore=response_body['weapon_lore'], location_description=response_body['location_description'])
+        db.session.add(exoticWeapons)
+        db.session.commit()
+        return jsonify(exoticWeapons.serialize()), 200
+    elif type(response_body)==list:
+        for item in response_body:
+            exoticWeapons=ExoticWeapon(weapon_name=item['weapon_name'], weapon_type=item['weapon_type'],weapon_class=item['weapon_class'], weapon_lore=item['weapon_lore'], location_description=item['location_description'])
+            db.session.add(exoticWeapons)
+            db.session.commit()
+        return "Exotics succesfully added", 200
 
 @api.route('/exotics/<int:exotics_id>', methods=['DELETE'])
 def delexoticsId(exotics_id):
@@ -128,10 +159,20 @@ def getlegendaryId(legendary_id):
 @api.route('/legendary', methods=['POST'])
 def access_legendaryweapons():
     response_body = request.get_json()
-    weaponInput=LegendaryWeapon(weapon_name=response_body['weapon_name'], weapon_type=response_body['weapon_type'],weapon_class=response_body['weapon_class'], weapon_lore=response_body['weapon_lore'], location_description=response_body['location_description'])
-    db.session.add(weaponInput)
-    db.session.commit()
-    return jsonify(weaponInput.serialize()), 200
+    if response_body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+        print(response_body)
+    if type(response_body)==dict:
+        weaponInput=LegendaryWeapon(weapon_name=response_body['weapon_name'], weapon_type=response_body['weapon_type'],weapon_class=response_body['weapon_class'], weapon_lore=response_body['weapon_lore'], location_description=response_body['location_description'])
+        db.session.add(weaponInput)
+        db.session.commit()
+        return jsonify(weaponInput.serialize()), 200
+    elif type(response_body)==list:
+        for item in response_body:
+            weaponInput=LegendaryWeapon(weapon_name=item['weapon_name'], weapon_type=item['weapon_type'],weapon_class=item['weapon_class'], weapon_lore=item['weapon_lore'], location_description=item['location_description'])
+            db.session.add(weaponInput)
+            db.session.commit()
+        return "SUccefully added Legendary Weapons", 200
 
 @api.route('/legendary/<int:legendary_id>', methods=['DELETE'])
 def legendaryId(legendary_id):
@@ -160,10 +201,20 @@ def getrareId(rare_id):
 @api.route('/rare', methods=['POST'])
 def access_rareweapons():
     response_body = request.get_json()
-    weaponInput=RareWeapon(weapon_name=response_body['weapon_name'], weapon_type=response_body['weapon_type'],weapon_class=response_body['weapon_class'], weapon_lore=response_body['weapon_lore'], location_description=response_body['location_description'])
-    db.session.add(weaponInput)
-    db.session.commit()
-    return jsonify(weaponInput.serialize()), 200
+    if response_body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+        print(response_body)
+    if type(response_body)==dict:
+        weaponInput=RareWeapon(weapon_name=response_body['weapon_name'], weapon_type=response_body['weapon_type'],weapon_class=response_body['weapon_class'], weapon_lore=response_body['weapon_lore'], location_description=response_body['location_description'])
+        db.session.add(weaponInput)
+        db.session.commit()
+        return jsonify(weaponInput.serialize()), 200
+    elif type(response_body)==list:
+        for item in response_body:
+            weaponInput=RareWeapon(weapon_name=item['weapon_name'], weapon_type=item['weapon_type'],weapon_class=item['weapon_class'], weapon_lore=item['weapon_lore'], location_description=item['location_description'])
+            db.session.add(weaponInput)
+            db.session.commit()
+        return "Succesfully posted Rare Weapons", 200
 
 @api.route('/rare/<int:rare_id>', methods=['DELETE'])
 def rareId(rare_id):
@@ -191,10 +242,20 @@ def getuncommonId(uncommon_id):
 @api.route('/uncommon', methods=['POST'])
 def access_uncommonweapons():
     response_body = request.get_json()
-    weaponInput=UncommonWeapon(weapon_name=response_body['weapon_name'], weapon_type=response_body['weapon_type'],weapon_class=response_body['weapon_class'], weapon_lore=response_body['weapon_lore'], location_description=response_body['location_description'])
-    db.session.add(weaponInput)
-    db.session.commit()
-    return jsonify(weaponInput.serialize()), 200
+    if response_body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+        print(response_body)
+    if type(response_body)==dict:
+        weaponInput=UncommonWeapon(weapon_name=response_body['weapon_name'], weapon_type=response_body['weapon_type'],weapon_class=response_body['weapon_class'], weapon_lore=response_body['weapon_lore'], location_description=response_body['location_description'])
+        db.session.add(weaponInput)
+        db.session.commit()
+        return jsonify(weaponInput.serialize()), 200
+    elif type(response_body)==list:
+        for item in response_body:
+            weaponInput=UncommonWeapon(weapon_name=item['weapon_name'], weapon_type=item['weapon_type'],weapon_class=item['weapon_class'], weapon_lore=item['weapon_lore'], location_description=item['location_description'])
+            db.session.add(weaponInput)
+            db.session.commit()
+        return "Succesfully added Uncommon Weapons", 200
 
 @api.route('/uncommon/<int:uncommon_id>', methods=['DELETE'])
 def uncommonId(uncommon_id):
@@ -224,10 +285,20 @@ def getcommonId(common_id):
 @api.route('/common', methods=['POST'])
 def access_commonweapons():
     response_body = request.get_json()
-    weaponInput=CommonWeapon(weapon_name=response_body['weapon_name'], weapon_type=response_body['weapon_type'],weapon_class=response_body['weapon_class'], weapon_lore=response_body['weapon_lore'], location_description=response_body['location_description'])
-    db.session.add(weaponInput)
-    db.session.commit()
-    return jsonify(weaponInput.serialize()), 200
+    if response_body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+        print(response_body)
+    if type(response_body)==dict:
+        weaponInput=CommonWeapon(weapon_name=response_body['weapon_name'], weapon_type=response_body['weapon_type'],weapon_class=response_body['weapon_class'], weapon_lore=response_body['weapon_lore'], location_description=response_body['location_description'])
+        db.session.add(weaponInput)
+        db.session.commit()
+        return jsonify(weaponInput.serialize()), 200
+    elif type(response_body)==list:
+        for item in response_body:
+            weaponInput=CommonWeapon(weapon_name=item['weapon_name'], weapon_type=item['weapon_type'],weapon_class=item['weapon_class'], weapon_lore=item['weapon_lore'], location_description=item['location_description'])
+            db.session.add(weaponInput)
+            db.session.commit()
+        return "Successfully added Common Weapons", 200
 
 @api.route('/common/<int:common_id>', methods=['DELETE'])
 def commonId(common_id):
