ca71c85163866e851954ebc45e5afa8d4db0a551
marcuslouis <Louis.Marcus@live.jobcorps.org>
2022-05-27 19:26:08 +0000

    all

--- src/front/js/component/Signin.jsx
+++ src/front/js/component/Signin.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { useHistory } from "react-router-dom";
-// import Images from "../../img/warmindImg.jpg";
+import Images from "../../img/main2.jpg";
 // IF YOU USE FLUX, YOU USE USECONTEXT, BECAUSE YOU ARE USEING THE STORE
 export const SignIn = () => {
   const [email, setEmail] = React.useState("");
@@ -10,7 +10,7 @@ export const SignIn = () => {
   const { store, actions } = useContext(Context);
   const [passLogin, setPasslogin] = useState(false);
   let history = useHistory();
-  // let blob = [Images];
+  let blob = [Images];
 
   return (
     <div>
@@ -19,13 +19,19 @@ export const SignIn = () => {
         style={{
           position: "absolute",
           left: "25%",
-          marginTop: "10%",
+          marginTop: "5%",
           width: "40%",
           borderRadius: "2%",
           height: "30%",
         }}
       >
-        <div style={{ marginTop: "11%" }}>
+        <img
+          src={blob}
+          className="img-fluid"
+          style={{ width: "100%" }}
+          alt="..."
+        />
+        <div style={{ marginTop: "1%" }}>
           <div className="mb-3">
             <input
               onChange={(e) => setEmail(e.target.value)}
@@ -37,49 +43,44 @@ export const SignIn = () => {
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
             />
+
             <div id="emailHelp" className="form-text text-light">
               LETS LOGIN
             </div>
+            <div className="mb-3">
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                style={{ width: "50%" }}
+                placeholder="Password"
+                type="password"
+                className="form-control move"
+                id="exampleInputPassword1"
+              />
+            </div>
           </div>
-          <div className="mb-3">
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              style={{ width: "50%" }}
-              placeholder="Password"
-              type="password"
-              className="form-control move"
-              id="exampleInputPassword1"
-            />
-          </div>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              if (email === "" || password === "") {
+                alert("The input cannot be empty");
+              }
+              //NEED ANOTHER IF STATEMENT
+              else {
+                actions.signIn(email, password);
+                history.push("/all");
+              }
+            }}
+            formtype="submit"
+            className="btn btn-primary"
+          >
+            Submit
+          </button>
+          <Link to="/all">
+            <button className="btn btn-primary">Back home</button>
+          </Link>
         </div>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            if (email === "" || password === "") {
-              alert("The input cannot be empty");
-            }
-            //NEED ANOTHER IF STATEMENT
-            else {
-              actions.signIn(email, password);
-              history.push("/all");
-            }
-          }}
-          formtype="submit"
-          className="btn btn-primary"
-        >
-          Submit
-        </button>
-        <Link to="/all">
-          <button className="btn btn-primary">Back home</button>
-        </Link>
       </form>
-      {/* <img
-        src={blob}
-        className="img-fluid"
-        style={{ width: "100%" }}
-        alt="..."
-      /> */}
     </div>
   );
 };
