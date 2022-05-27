1cfbc791913ac469231591cf355b65023110faa4
55746 <Lawhead.gavin@live.jobcorps.org>
2022-05-27 14:50:27 +0000

    found a pic for search bar

--- src/front/js/component/all.jsx
+++ src/front/js/component/all.jsx
@@ -3,15 +3,19 @@ import { Exotics } from "./exotics.jsx";
 import { Legendary } from "././legendary.jsx";
 import { Rare } from "././rare.jsx";
 import { Uncommon } from "././uncommon.jsx";
+import TESTIMAGE from "../../img/destiny_nightfall_weapon_adept.jpg";
 
 export const All = () => {
   // let blob = [Images];
   return (
-    <div>
-      <Exotics />
-      <Legendary />
-      <Rare />
-      <Uncommon />
+    <div className="row" style={{ justifyContent: "center" }}>
+      <img src={TESTIMAGE} />
+      <div>
+        <Exotics />
+        <Legendary />
+        <Rare />
+        <Uncommon />
+      </div>
     </div>
   );
 };
