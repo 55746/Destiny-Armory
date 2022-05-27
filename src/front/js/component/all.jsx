d60d71f87076dfe2da7a2d89ad155aa207bd95cd
marcuslouis <Louis.Marcus@live.jobcorps.org>
2022-05-27 15:59:36 +0000

    all

--- src/front/js/component/all.jsx
+++ src/front/js/component/all.jsx
@@ -3,22 +3,37 @@ import { Exotics } from "./exotics.jsx";
 import { Legendary } from "././legendary.jsx";
 import { Rare } from "././rare.jsx";
 import { Uncommon } from "././uncommon.jsx";
-import TESTIMAGE from "../../img/destiny_nightfall_weapon_adept.jpg";
-// import background from "../../img/Destiny - Moon 1.png";
-
+import Images from "../../img/main2.jpg";
 export const All = () => {
-  // let blob = [Images];
+  let blob = [Images];
   return (
     <div>
-      <div className="row" style={{ justifyContent: "center" }}>
-        <img src={TESTIMAGE} />
-        <div>
-          <Exotics />
-          <Legendary />
-          <Rare />
-          <Uncommon />
+      <img src={blob} style={{ width: "100%", height: "100%" }} />
+      <div
+        style={{
+          position: "absolute",
+          left: "40%",
+          marginTop: "-35%",
+          width: "50%",
+        }}
+        className="input-group"
+      >
+        <div className="form-outline">
+          <input type="search" id="form1" className="form-control" />
+          <label className="form-label" for="form1"></label>
         </div>
+        <button
+          type="button"
+          className="btn btn-primary"
+          style={{ position: "absolute", left: "25%" }}
+        >
+          search
+        </button>
       </div>
+      <Exotics />
+      <Legendary />
+      <Rare />
+      <Uncommon />
     </div>
   );
 };
